8a2754ba185bcbc882a26b9fb8fe3731ff60d99c
metfor <mateus_frade@hotmail.com>
2021-05-27 09:47:49 -0300

    3/5

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Container } from "./styles";
 import { Summary } from "../Summary";
+import { TransectionsTable } from "../TransectionsTable";
 
 export function Dashboard(){
     return(
         <Container>
             <Summary/>
+            <TransectionsTable/>
         </Container>
     )
 }
